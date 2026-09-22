0523016ecd9545d152991f300d48fe76d59dae57
Matviaoli <96087731+Matviaoli@users.noreply.github.com>
2026-09-22 14:16:11 -0300

    indev0.0.2.2
    I swear, i'm trying my beast

--- Scripts/TileLayer/TerrainLayerScript.gd
+++ Scripts/TileLayer/TerrainLayerScript.gd
@@ -8,6 +8,5 @@ extends BaseTileMapLayer
 # ====================
 # Can place
 # ====================
-# O terreno pode ser trocado por cima, só exige que o id exista
 func can_place(_cell: Vector2i, id: StringName) -> bool:
 	return definitions.has(id)
--- Scripts/World Scripts/WorldBaseScript.gd
+++ Scripts/World Scripts/WorldBaseScript.gd
@@ -1,45 +1,5 @@
 ## ====================
 ## Class & Extend
 ## ====================
-class_name WorldBaseScript
+class_name WorldBase
 extends Node2D
-
-
-## ====================
-## Data
-## ====================
-@export var flat_size := Vector2i(24, 24)
-@export var starting_terrain_id: StringName = &"grass"
-
-
-## ====================
-## Refs (nomes dos nós na cena World.tscn)
-## ====================
-@onready var terrain: TerrainLayer = $TerrainLayer
-@onready var structures: StructureLayer = $StructureLayer
-@onready var simulation: Simulation = $Simulation
-@onready var build_input: BuildInput = $BuildInput
-
-
-## ====================
-## Wake up
-## ====================
-func _ready() -> void:
-	# Conecta as camadas entre si (não dá pra fazer isso 100% pelo Inspector
-	# porque terrainLayer/structures são var normais, não @export tipadas
-	# aceitando NodePath direto)
-	structures.terrainLayer = terrain
-	simulation.structures = structures
-	build_input.structures = structures
-	build_input.simulation = simulation
-
-	_generate_flat_world()
-
-
-## ====================
-## Geração (manual/flat por enquanto)
-## ====================
-func _generate_flat_world() -> void:
-	for x in range(flat_size.x):
-		for y in range(flat_size.y):
-			terrain.place(Vector2i(x, y), starting_terrain_id)
